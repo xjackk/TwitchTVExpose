cbaef4ee296965da0481cbe59e7f48fca643128e
Jack Killilea <xjackk@live.com>
2013-09-09 19:59:57 -0400

    making sure nodebuildapp.sh works from cmd line

--- nodebuildapp.sh
+++ nodebuildapp.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-# $ nodebuild
-#echo "Building optimized javascript library for twitchtv"
-#echo "using r.js optimizer"
-node bower_components/r.js/dist/r.js -o ./build.js
-#echo "finished"
-#echo "output in ZIRP/js/main.optimized.js"
+exec node bower_components/r.js/dist/r.js -o build.js
